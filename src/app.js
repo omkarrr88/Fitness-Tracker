8917430312e661a16de4973d68298ba2ddc52294
Omkar <devilomkar88@gmail.com>
2024-10-19 18:17:22 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use('/api', workoutRoutes); // Add this line
 
 // Serve HTML files
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'main.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.get('/account', (req, res) => {
